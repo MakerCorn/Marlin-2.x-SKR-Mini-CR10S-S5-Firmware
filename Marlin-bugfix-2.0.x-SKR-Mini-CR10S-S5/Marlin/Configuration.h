d7851bb639bd9580eeb5504bd76b08bfed663721
Corn Pivotal <cmendoza@pivotal.io>
2019-11-15 17:36:22 -0600

    update bed dimensions

--- Marlin-bugfix-2.0.x-SKR-Mini-CR10S-S5/Marlin/Configuration.h
+++ Marlin-bugfix-2.0.x-SKR-Mini-CR10S-S5/Marlin/Configuration.h
@@ -1049,8 +1049,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 510 
-#define Y_BED_SIZE 510 
+#define X_BED_SIZE 500 
+#define Y_BED_SIZE 500 
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
